ec4a5670e7075e87b655ed55d95ae29628da5079
Kevin Li <likevinhp@gmail.com>
2023-06-14 19:13:28 +0000

    Removed icon-container div and tweaked overlay

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const $agentIconsRow = document.querySelector('.agent-icons');
 const $filter = document.querySelector('.roles-icons');
 const $roleButtons = document.querySelectorAll('.role-icon');
+let $agentIcons;
 
-function getAgents(language, isPlayable) {
+function getAgents() {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://valorant-api.com/v1/agents');
   xhr.responseType = 'json';
@@ -11,14 +12,14 @@ function getAgents(language, isPlayable) {
       const resp = xhr.response.data;
       for (let i = 0; i < resp.length; i++) {
         if (resp[i].isPlayableCharacter === true) {
-          const $div = document.createElement('div');
+          // const $div = document.createElement('div');
           const $agent = document.createElement('img');
-          $div.setAttribute('class', 'icon-container');
+          // $div.setAttribute('class', 'icon-container');
           $agent.setAttribute('src', resp[i].displayIcon);
           $agent.setAttribute('class', 'agent-icon');
-          $agent.classList.add('class', resp[i].role.displayName.toLowerCase());
-          $agentIconsRow.append($div);
-          $div.append($agent);
+          $agent.classList.add(resp[i].role.displayName.toLowerCase());
+          $agentIconsRow.append($agent);
+          // $div.append($agent);
         }
       }
     }
@@ -32,7 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 $filter.addEventListener('click', () => {
-
   if (event.target.classList.contains('current')) {
     event.target.classList.remove('current');
     for (let i = 0; i < $roleButtons.length; i++) {
@@ -50,4 +50,13 @@ $filter.addEventListener('click', () => {
       event.target.classList.add('current');
     }
   }
+
+  $agentIcons = document.querySelectorAll('.agent-icon');
+
+  for (let i = 0; i < $agentIcons.length; i++) {
+    if (!$agentIcons[i].classList.contains(event.target.id)) {
+      $agentIcons[i].classList.add('hidden');
+    }
+  }
+
 });

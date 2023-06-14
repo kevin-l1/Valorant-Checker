8e81a14feddbbea80352b22ef74fc4f2e320c0b3
Kevin Li <likevinhp@gmail.com>
2023-06-14 07:24:02 +0000

    Corrected issues regarding screen overlay and default size begins with mobile

--- js/main.js
+++ js/main.js
@@ -1,36 +1,4 @@
 const $agentIconsRow = document.querySelector('.agent-icons');
-// const $filter = document.querySelector('.roles-icons');
-
-// function getAgents(language, isPlayable) {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'https://valorant-api.com/v1/agents');
-//   xhr.responseType = 'json';
-//   xhr.addEventListener('load', () => {
-//     console.log(xhr.status);
-//     console.log(xhr.response);
-//     if (xhr.response && xhr.response.data) {
-//       const resp = xhr.response.data;
-//       for (let i = 0; i < resp.length; i++) {
-//         if (resp[i].isPlayableCharacter === true) {
-//           const $agent = document.createElement('div');
-//           // $agent.setAttribute('src', resp[i].displayIcon);
-//           $agent.setAttribute('class', 'circle');
-//           const $icon = document.createElement('img');
-//           $icon.setAttribute('src', resp[i].displayIcon);
-//           $icon.setAttribute('class', 'agent-icon');
-//           $icon['background-image'] = resp[i].displayIcon;
-//           $agentIconsRow.append($agent);
-//           // $agent.append($icon);
-//         }
-//       }
-//     } else {
-//       // error handler
-//     }
-
-//   });
-//   xhr.send();
-// }
-// getAgents();
 
 function getAgents(language, isPlayable) {
   const xhr = new XMLHttpRequest();
@@ -50,35 +18,9 @@ function getAgents(language, isPlayable) {
           $div.append($agent);
         }
       }
-    } else {
-      // error handler
     }
 
   });
   xhr.send();
 }
 getAgents();
-
-// function getWeapons() {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'https://valorant-api.com/v1/weapons');
-//   xhr.responseType = 'json';
-//   xhr.addEventListener('load', () => {
-//     console.log(xhr.status);
-//     console.log(xhr.response);
-//     if (xhr.response && xhr.response.data) {
-//       const resp = xhr.response.data;
-//       for (let i = 0; i < resp.length; i++) {
-//         let $div = document.createElement('div')
-//         $div.textContent = resp[i].displayName;
-//         $body.append($div);
-//       }
-//     } else {
-//       // error handler
-//       console.log('hi')
-//     }
-
-//   })
-//     xhr.send();
-// }
-// getWeapons()

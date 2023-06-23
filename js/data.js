b0ec288aa393798fde9c5517b16ad7bfa0dd003c
Kevin Li <likevinhp@gmail.com>
2023-06-23 06:58:53 +0000

    Fixed add bookmark functionality

--- js/data.js
+++ js/data.js
@@ -1,1 +1,16 @@
 /* exported data */
+
+var data = {
+  bookmarkedAgents: [],
+  nextEntryId: 1
+};
+
+window.addEventListener('beforeunload', () => {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJSON);
+});
+
+const previousDataJSON = localStorage.getItem('javascript-local-storage');
+if (previousDataJSON != null) {
+  data = JSON.parse(previousDataJSON);
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-const $agentIconsRow = document.querySelector('.agent-icons');
+const $agentIconsRow = document.querySelector('.home-icons');
 const $filter = document.querySelector('.roles-icons');
 const $roleButtons = document.querySelectorAll('.role-icon');
 const $agentPageRow = document.querySelector('.agent-pages');
-let $agentIcons = document.querySelector('.agent-icons');
+let $agentIcons;
+const $bookmarksTab = document.querySelector('.bookmarks-tab');
 
 function getAgents() {
   const xhr = new XMLHttpRequest();
@@ -30,7 +31,12 @@ function getAgents() {
           const $abilities = document.createElement('div');
           $abilities.setAttribute('class', 'column-full right');
           const $bookmarkIcon = document.createElement('i');
-          $bookmarkIcon.setAttribute('class', 'fa-regular fa-bookmark');
+
+          if (data.bookmarkedAgents.includes(i)) {
+            $bookmarkIcon.setAttribute('class', 'fa-solid fa-bookmark');
+          } else {
+            $bookmarkIcon.setAttribute('class', 'fa-regular fa-bookmark');
+          }
 
           const $portraitText = document.createElement('div');
           $portraitText.setAttribute('class', 'portrait-text');
@@ -139,8 +145,133 @@ function getAgents() {
   xhr.send();
 }
 
+function renderAgentIcon(resp, id) {
+  const $agentIcon = document.createElement('img');
+  $agentIcon.setAttribute('src', resp[id].displayIcon);
+  $agentIcon.setAttribute('class', 'agent-icon');
+  $agentIcon.classList.add('bookmarked-icon');
+  $agentIcon.setAttribute('alt', 'Agent Icon');
+  $agentIcon.classList.add(resp[id].role.displayName.toLowerCase());
+
+  return $agentIcon;
+}
+
+function renderAgentPage(resp, id) {
+  const $agentPage = document.createElement('div');
+  $agentPage.setAttribute('class', 'row');
+  $agentPage.classList.add('bookmarked-agent-page');
+  $agentPage.classList.add('hidden');
+  const $describedImage = document.createElement('div');
+  $describedImage.setAttribute('class', 'column-full left');
+  const $abilities = document.createElement('div');
+  $abilities.setAttribute('class', 'column-full right');
+  const $bookmarkIcon = document.createElement('i');
+  $bookmarkIcon.setAttribute('class', 'fa-solid fa-bookmark');
+
+  const $portraitText = document.createElement('div');
+  $portraitText.setAttribute('class', 'portrait-text');
+  const $portrait = document.createElement('img');
+  $portrait.setAttribute('src', resp[id].fullPortrait);
+  $portrait.setAttribute('class', 'agent-portrait');
+  $portrait.setAttribute('alt', 'Agent Portrait');
+  const $name = document.createElement('h1');
+  $name.setAttribute('class', 'name');
+  $name.textContent = resp[id].displayName;
+  const $description = document.createElement('p');
+  $description.setAttribute('class', 'agent-description');
+  $description.textContent = resp[id].description;
+
+  const $abilityOne = document.createElement('div');
+  $abilityOne.setAttribute('class', 'ability');
+  const $abilityTwo = document.createElement('div');
+  $abilityTwo.setAttribute('class', 'ability');
+  const $abilityThree = document.createElement('div');
+  $abilityThree.setAttribute('class', 'ability');
+  const $ultimate = document.createElement('div');
+  $ultimate.setAttribute('class', 'ability');
+
+  const $abilityOneIcon = document.createElement('img');
+  $abilityOneIcon.setAttribute('class', 'ability-icon');
+  $abilityOneIcon.setAttribute('alt', 'Ability One Icon');
+  $abilityOneIcon.setAttribute('src', resp[id].abilities[0].displayIcon);
+  const $abilityTwoIcon = document.createElement('img');
+  $abilityTwoIcon.setAttribute('class', 'ability-icon');
+  $abilityTwoIcon.setAttribute('src', resp[id].abilities[1].displayIcon);
+  $abilityTwoIcon.setAttribute('alt', 'Ability Two Icon');
+  const $abilityThreeIcon = document.createElement('img');
+  $abilityThreeIcon.setAttribute('class', 'ability-icon');
+  $abilityThreeIcon.setAttribute('alt', 'Ability Three Icon');
+  $abilityThreeIcon.setAttribute('src', resp[id].abilities[2].displayIcon);
+  const $ultimateIcon = document.createElement('img');
+  $ultimateIcon.setAttribute('class', 'ability-icon');
+  $ultimateIcon.setAttribute('alt', 'Ultimate Icon');
+  $ultimateIcon.setAttribute('src', resp[id].abilities[3].displayIcon);
+
+  const $abilityOneName = document.createElement('h2');
+  $abilityOneName.textContent = resp[id].abilities[0].displayName;
+  const $abilityTwoName = document.createElement('h2');
+  $abilityTwoName.textContent = resp[id].abilities[1].displayName;
+  const $abilityThreeName = document.createElement('h2');
+  $abilityThreeName.textContent = resp[id].abilities[2].displayName;
+  const $ultimateName = document.createElement('h2');
+  $ultimateName.textContent = resp[id].abilities[3].displayName;
+
+  const $abilityOneDescription = document.createElement('p');
+  $abilityOneDescription.textContent = resp[id].abilities[0].description;
+  const $abilityTwoDescription = document.createElement('p');
+  $abilityTwoDescription.textContent = resp[id].abilities[1].description;
+  const $abilityThreeDescription = document.createElement('p');
+  $abilityThreeDescription.textContent = resp[id].abilities[2].description;
+  const $ultimateDescription = document.createElement('p');
+  $ultimateDescription.textContent = resp[id].abilities[3].description;
+
+  const $abilityOneText = document.createElement('div');
+  $abilityOneText.setAttribute('class', 'ability-text');
+  $abilityOneText.append($abilityOneName);
+  $abilityOneText.append($abilityOneDescription);
+  const $abilityTwoText = document.createElement('div');
+  $abilityTwoText.setAttribute('class', 'ability-text');
+  $abilityTwoText.append($abilityTwoName);
+  $abilityTwoText.append($abilityTwoDescription);
+  const $abilityThreeText = document.createElement('div');
+  $abilityThreeText.setAttribute('class', 'ability-text');
+  $abilityThreeText.append($abilityThreeName);
+  $abilityThreeText.append($abilityThreeDescription);
+  const $ultimateText = document.createElement('div');
+  $ultimateText.setAttribute('class', 'ability-text');
+  $ultimateText.append($ultimateName);
+  $ultimateText.append($ultimateDescription);
+
+  $portraitText.append($name);
+  $portraitText.append($description);
+
+  $abilityOne.append($abilityOneIcon);
+  $abilityOne.append($abilityOneText);
+  $abilityTwo.append($abilityTwoIcon);
+  $abilityTwo.append($abilityTwoText);
+  $abilityThree.append($abilityThreeIcon);
+  $abilityThree.append($abilityThreeText);
+  $ultimate.append($ultimateIcon);
+  $ultimate.append($ultimateText);
+
+  $describedImage.append($portrait);
+  $describedImage.append($portraitText);
+
+  $abilities.append($abilityOne);
+  $abilities.append($abilityTwo);
+  $abilities.append($abilityThree);
+  $abilities.append($ultimate);
+
+  $agentPage.append($describedImage);
+  $agentPage.append($abilities);
+  $agentPage.append($bookmarkIcon);
+
+  return $agentPage;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   getAgents();
+  renderBookmark();
 });
 
 $filter.addEventListener('click', () => {
@@ -182,7 +313,8 @@ let $allIcons;
 let $agentPages;
 let index;
 
-$agentIcons.addEventListener('click', () => {
+$agentIconsRow.addEventListener('click', () => {
+
   $allIcons = document.querySelectorAll('.agent-icon');
   $agentPages = document.querySelectorAll('.agent-page');
   if (!$bookmarksRow.classList.contains('hidden')) {
@@ -193,6 +325,9 @@ $agentIcons.addEventListener('click', () => {
   }
   if (!$agentIconsRow.classList.contains('hidden')) {
     $agentIconsRow.classList.add('hidden');
+  }
+  if ($agentPageRow.classList.contains('hidden')) {
+    $agentPageRow.classList.remove('hidden');
   }
   for (index = 0; index < $allIcons.length; index++) {
     if (event.target === $allIcons[index]) {
@@ -224,59 +359,116 @@ $agentsTab.addEventListener('click', () => {
       $allIcons[i].classList.remove('hidden');
     }
   }
-
 });
 
 const $logo = document.querySelector('.logo');
 
 $logo.addEventListener('click', () => {
+  $agentPages = document.querySelectorAll('.agent-page');
+
   if (!$bookmarksRow.classList.contains('hidden')) {
     $bookmarksRow.classList.add('hidden');
   }
-  $agentPages = document.querySelectorAll('.agent-page');
-  $agentPages[index].classList.add('hidden');
+  if ($agentPages[index]) {
+    if (!$agentPages[index].classList.contains('hidden')) {
+      $agentPages[index].classList.add('hidden');
+    }
+  }
   $filter.classList.remove('hidden');
   $agentIconsRow.classList.remove('hidden');
+  for (let i = 0; i < $allIcons.length; i++) {
+    if ($allIcons[i].classList.contains('hidden')) {
+      $allIcons[i].classList.remove('hidden');
+    }
+  }
 });
 
 let bookmarkNum;
 
-$agentPageRow.addEventListener('click', () => {
+function bookmark() {
   const $bookmarkAll = document.querySelectorAll('.fa-bookmark');
   $agentPages = document.querySelectorAll('.agent-page');
   $allIcons = document.querySelectorAll('.agent-icon');
   for (bookmarkNum = 0; bookmarkNum < $bookmarkAll.length; bookmarkNum++) {
     if (event.target === $bookmarkAll[bookmarkNum]) {
+      data.bookmarkedAgents.push(bookmarkNum);
       $bookmarkAll[bookmarkNum].setAttribute('class', 'fa-solid fa-bookmark');
-      $agentPages[bookmarkNum].classList.add('bookmarked');
-      $allIcons[bookmarkNum].classList.add('bookmarked');
       return;
     }
   }
-});
+}
 
-const $bookmarksTab = document.querySelector('.bookmarks-tab');
-const $bookmarksRow = document.querySelector('.bookmarks');
+$agentPageRow.addEventListener('click', bookmark);
 
 $bookmarksTab.addEventListener('click', () => {
   $agentPages = document.querySelectorAll('.agent-page');
   $allIcons = document.querySelectorAll('.agent-icon');
+  const $AllBookmarkedIcons = document.querySelectorAll('.bookmarked-icon');
+  const $AllBookmarkedPages = document.querySelectorAll('.bookmarked-agent-page');
 
   if ($bookmarksRow.classList.contains('hidden')) {
     $bookmarksRow.classList.remove('hidden');
   }
+  if ($bookmarksText.classList.contains('hidden')) {
+    $bookmarksText.classList.remove('hidden');
+  }
+  if ($bookmarkedIconsRow.classList.contains('hidden')) {
+    $bookmarkedIconsRow.classList.remove('hidden');
+  }
   if (!$filter.classList.contains('hidden')) {
     $filter.classList.add('hidden');
   }
-  if ($agentIconsRow.classList.contains('hidden')) {
-    $agentIconsRow.classList.remove('hidden');
+  if (!$agentIconsRow.classList.contains('hidden')) {
+    $agentIconsRow.classList.add('hidden');
   }
-  for (let i = 0; i < $allIcons.length; i++) {
-    if (!$allIcons[i].classList.contains('bookmarked')) {
-      $allIcons[i].classList.add('hidden');
+  if (!$agentPageRow.classList.contains('hidden')) {
+    $agentPageRow.classList.add('hidden');
+  }
+  for (let i = 0; i < $AllBookmarkedIcons.length; i++) {
+    if (!$AllBookmarkedPages[i].classList.contains('hidden')) {
+      $AllBookmarkedPages[i].classList.add('hidden');
     }
-    if (!$agentPages[i].classList.contains('hidden')) {
-      $agentPages[i].classList.add('hidden');
+  }
+});
+
+const $bookmarkedIconsRow = document.querySelector('.bookmarked-agent-icons');
+const $bookmarkedPagesRow = document.querySelector('.bookmarked-agent-pages');
+
+function renderBookmark() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://valorant-api.com/v1/agents');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', () => {
+    if (xhr.response && xhr.response.data) {
+      const resp = xhr.response.data;
+      for (let i = 0; i < data.bookmarkedAgents.length; i++) {
+        $bookmarkedIconsRow.append(renderAgentIcon(resp, data.bookmarkedAgents[i]));
+        $bookmarkedPagesRow.append(renderAgentPage(resp, data.bookmarkedAgents[i]));
+      }
+    }
+  });
+  xhr.send();
+}
+
+const $bookmarksRow = document.querySelector('.bookmarks');
+const $bookmarksText = document.querySelector('.bookmarks-text');
+
+$bookmarksRow.addEventListener('click', () => {
+  const $AllBookmarkedIcons = document.querySelectorAll('.bookmarked-icon');
+  const $AllBookmarkedPages = document.querySelectorAll('.bookmarked-agent-page');
+
+  if (!$bookmarksText.classList.contains('hidden')) {
+    $bookmarksText.classList.add('hidden');
+  }
+  if (!$bookmarkedIconsRow.classList.contains('hidden')) {
+    $bookmarkedIconsRow.classList.add('hidden');
+  }
+  for (let i = 0; i < $AllBookmarkedIcons.length; i++) {
+    if (event.target === $AllBookmarkedIcons[i]) {
+      if ($AllBookmarkedPages[i].classList.contains('hidden')) {
+        $AllBookmarkedPages[i].classList.remove('hidden');
+        return;
+      }
     }
   }
 });
